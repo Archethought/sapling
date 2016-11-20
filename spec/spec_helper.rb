4de29357e35fb93bc94bd651f2d3bce5b2e1ba35
Chris Boertien <chris.boertien@gmail.com>
2016-11-20 09:35:28 -0400

    reset Sapling after each test runs
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "sapling"
+
+Dir["#{Dir.pwd}/spec/lib/*.rb"].each(&method(:require))
+
+RSpec.configure do |c|
+  c.after(:each) do
+    Sapling.reset_configuration
+  end
+end
